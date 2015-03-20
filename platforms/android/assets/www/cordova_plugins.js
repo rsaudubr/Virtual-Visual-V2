e8c2a636a72def391a6ed9f4714a6da138729197
rsaudubr <eolen9@gmail.com>
2015-03-20 11:19:42 +0000

    Application V2 First commit
    This application works on my phone (except using audio file)

---
+++ platforms/android/assets/www/cordova_plugins.js
@@ -0,0 +1,214 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/org.apache.cordova.file/www/DirectoryEntry.js",
+        "id": "org.apache.cordova.file.DirectoryEntry",
+        "clobbers": [
+            "window.DirectoryEntry"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.file/www/DirectoryReader.js",
+        "id": "org.apache.cordova.file.DirectoryReader",
+        "clobbers": [
+            "window.DirectoryReader"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.file/www/Entry.js",
+        "id": "org.apache.cordova.file.Entry",
+        "clobbers": [
+            "window.Entry"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.file/www/File.js",
+        "id": "org.apache.cordova.file.File",
+        "clobbers": [
+            "window.File"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.file/www/FileEntry.js",
+        "id": "org.apache.cordova.file.FileEntry",
+        "clobbers": [
+            "window.FileEntry"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.file/www/FileError.js",
+        "id": "org.apache.cordova.file.FileError",
+        "clobbers": [
+            "window.FileError"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.file/www/FileReader.js",
+        "id": "org.apache.cordova.file.FileReader",
+        "clobbers": [
+            "window.FileReader"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.file/www/FileSystem.js",
+        "id": "org.apache.cordova.file.FileSystem",
+        "clobbers": [
+            "window.FileSystem"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.file/www/FileUploadOptions.js",
+        "id": "org.apache.cordova.file.FileUploadOptions",
+        "clobbers": [
+            "window.FileUploadOptions"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.file/www/FileUploadResult.js",
+        "id": "org.apache.cordova.file.FileUploadResult",
+        "clobbers": [
+            "window.FileUploadResult"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.file/www/FileWriter.js",
+        "id": "org.apache.cordova.file.FileWriter",
+        "clobbers": [
+            "window.FileWriter"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.file/www/Flags.js",
+        "id": "org.apache.cordova.file.Flags",
+        "clobbers": [
+            "window.Flags"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.file/www/LocalFileSystem.js",
+        "id": "org.apache.cordova.file.LocalFileSystem",
+        "clobbers": [
+            "window.LocalFileSystem"
+        ],
+        "merges": [
+            "window"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.file/www/Metadata.js",
+        "id": "org.apache.cordova.file.Metadata",
+        "clobbers": [
+            "window.Metadata"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.file/www/ProgressEvent.js",
+        "id": "org.apache.cordova.file.ProgressEvent",
+        "clobbers": [
+            "window.ProgressEvent"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.file/www/fileSystems.js",
+        "id": "org.apache.cordova.file.fileSystems"
+    },
+    {
+        "file": "plugins/org.apache.cordova.file/www/requestFileSystem.js",
+        "id": "org.apache.cordova.file.requestFileSystem",
+        "clobbers": [
+            "window.requestFileSystem"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.file/www/resolveLocalFileSystemURI.js",
+        "id": "org.apache.cordova.file.resolveLocalFileSystemURI",
+        "merges": [
+            "window"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.file/www/android/FileSystem.js",
+        "id": "org.apache.cordova.file.androidFileSystem",
+        "merges": [
+            "FileSystem"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.file/www/fileSystems-roots.js",
+        "id": "org.apache.cordova.file.fileSystems-roots",
+        "runs": true
+    },
+    {
+        "file": "plugins/org.apache.cordova.file/www/fileSystemPaths.js",
+        "id": "org.apache.cordova.file.fileSystemPaths",
+        "merges": [
+            "cordova"
+        ],
+        "runs": true
+    },
+    {
+        "file": "plugins/org.apache.cordova.media/www/MediaError.js",
+        "id": "org.apache.cordova.media.MediaError",
+        "clobbers": [
+            "window.MediaError"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.media/www/Media.js",
+        "id": "org.apache.cordova.media.Media",
+        "clobbers": [
+            "window.Media"
+        ]
+    },
+    {
+        "file": "plugins/com.radlib.cordova.rfidreader/www/Resources.js",
+        "id": "com.radlib.cordova.rfidreader.Resources"
+    },
+    {
+        "file": "plugins/com.radlib.cordova.rfidreader/www/RadLib.js",
+        "id": "com.radlib.cordova.rfidreader.RadLib",
+        "clobbers": [
+            "radlib"
+        ]
+    },
+    {
+        "file": "plugins/com.radlib.cordova.rfidreader/www/InternalBarcodeScanner.js",
+        "id": "com.radlib.cordova.rfidreader.InternalBarcodeScanner"
+    },
+    {
+        "file": "plugins/com.radlib.cordova.rfidreader/www/CommBluetooth.js",
+        "id": "com.radlib.cordova.rfidreader.CommBluetooth"
+    },
+    {
+        "file": "plugins/com.radlib.cordova.rfidreader/www/BluetoothUtils.js",
+        "id": "com.radlib.cordova.rfidreader.BluetoothUtils",
+        "clobbers": [
+            "bluetoothUtils"
+        ]
+    },
+    {
+        "file": "plugins/com.radlib.cordova.rfidreader/www/ReaderArduino_RC522_HF.js",
+        "id": "com.radlib.cordova.rfidreader.ReaderArduino_RC522_HF"
+    },
+    {
+        "file": "plugins/com.radlib.cordova.rfidreader/www/ReaderTSL_1128_UHF.js",
+        "id": "com.radlib.cordova.rfidreader.ReaderTSL_1128_UHF"
+    },
+    {
+        "file": "plugins/com.phonegap.plugins.barcodescanner/www/barcodescanner.js",
+        "id": "com.phonegap.plugins.barcodescanner.BarcodeScanner",
+        "clobbers": [
+            "cordova.plugins.barcodeScanner"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "org.apache.cordova.file": "1.3.3",
+    "org.apache.cordova.media": "0.2.16",
+    "com.radlib.cordova.rfidreader": "0.0.0",
+    "com.phonegap.plugins.barcodescanner": "2.0.1"
+}
+// BOTTOM OF METADATA
+});
---
+++ platforms/android/assets/www/js/index.js
@@ -0,0 +1,150 @@
+
+var myMedia = null;
+
+
+function stopAudio() {
+    myMedia.stop();
+
+}
+
+function play(file) {
+    
+    var src = file;
+    // Clean up old file
+    if (myMedia != null) {
+        myMedia.release();
+    }
+
+
+
+    myMedia = new Media(src, stopAudio, null);
+    window.setTimeout(function() {
+        alert(myMedia.getDuration());
+        myMedia.play();
+    }, 500); 
+
+
+}
+
+function  fileDuration(file){
+    var src = 'stuff.mp3';
+    try {
+            myMedia = new Media(src, null, null, null);
+    }
+    catch(err) {
+        alert("En fait c'est une grosse blague qui me fait predre plein de jours...");
+    }
+    alert(Media);
+
+    alert(myMedia);
+    alert(myMedia.duration);
+}
+
+
+var app = {
+    // Application Constructor
+    initialize : function() {
+        this.bindEvents();
+    },
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    bindEvents : function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+    // deviceready Event Handler
+    //
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicitly call 'app.receivedEvent(...);'
+    onDeviceReady : function() {
+        
+        var tagEvent = document.getElementById("tagEvent");
+        
+        app.receivedEvent('deviceready');
+
+        // Read NDEF formatted NFC Tags
+        nfc.addNdefListener(function(nfcEvent) {
+            var tag = nfcEvent.tag, ndefMessage = tag.ndefMessage;
+
+            var tag = nfc.bytesToString(ndefMessage[0].payload).substring(3);
+            
+            tagEvent.innerHTML = '<p class="event tag">Tag detected : '+ tag +'</p>';
+            tagEvent.className = "fade-in";
+            
+            var tagTimeout = window.setTimeout(function(){
+                tagEvent.className = "fade-out";
+                
+            }, 2000);
+
+            console.log(tag);
+
+            switch (tag) {
+            case "1":
+                play('entrance.mp3');
+                break;
+            case "2":
+                play('metalwork.mp3');
+                break;
+            case "3":
+                play('science.mp3');
+                break;
+            case "4":
+                play('library.mp3');
+                break;
+            case "5":
+                play('woodwork.mp3');
+                break;
+            case "6":
+                play('reception.mp3');
+                break;
+            default:
+                console.log("unknown tag");
+                break;
+            }
+
+/*
+
+            if (tag == '12345') {
+
+                var audio_src = "/android_asset/www/audio/4.mp3";
+
+                function mediaError(e) {
+                    alert('Media Error');
+                    alert(JSON.stringify(e));
+                }
+
+                // alert("in the woodwork room");
+
+                // new Audio('/android_asset/www/audio/4.mp3').play();
+
+                //document.getElementById('demo').play()
+            } else if (tag == 'a12345') {
+                var audio_src = "/android_asset/www/audio/5.mp3";
+            } else {
+                alert("Somewhere else");
+            }
+
+            var media = new Media(audio_src, null, mediaError);
+            media.play();*/
+
+
+        }, function() {// success callback
+            // alert("Waiting for NDEF tag");
+        }, function(error) {// error callback
+            // alert("Error adding NDEF listener " + JSON.stringify(error));
+        });
+    },
+    // Update DOM on a Received Event
+    receivedEvent : function(id) {
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
+
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
+
+        console.log('Received Event: ' + id);
+    }
+
+};
